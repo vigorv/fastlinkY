207a53f9cab1113cbeee799fc205fa73183e357c
snow <snowman>
2012-04-29 16:46:52 +0700

    browser detect

--- protected/modules/admin/views/catalog/list.php
+++ protected/modules/admin/views/catalog/list.php
@@ -45,8 +45,9 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'id',
 		'user_id',
 		'email',
-		'title',
-		'original_name',
+//		'title',
+//		'original_name',
+        'sgroup',
 		'name',
 		/*
 		'comment',
