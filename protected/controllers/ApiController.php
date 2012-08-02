845b8f15560d8aaf04deefc16d944c05be0d9836
snow <snow@diamondc.ru>
2012-08-02 19:06:22 +0700

    move api actions to api controller

---
+++ protected/controllers/ApiController.php
@@ -0,0 +1,62 @@
+<?php
+
+class ApiController extends Controller
+{
+    public function beforeAction($action)
+    {
+        parent::beforeAction($action);
+        return true;
+    }
+
+    public function actionGroupOfFile($id=0){
+        if ($id > 0) {
+            $file = CFLCatalog::model()->cache(100)->findByPk($id);
+            $files = CFLCatalog::model()->cache(10)->findAllByAttributes(array('group'=>$file->group,'sgroup'=>$file->sgroup));
+            /** @var CFLCatalog $file */
+            if ($file && !empty($files)){
+                $data=array('group'=>$file->group,'count'=>count($files));
+                foreach ($files as $f)
+                    $data['files'][]=$f['id'];
+                echo serialize($data);
+            }
+        }
+    }
+
+    public function actionFilePath($item_id=0){
+        if ($item_id > 0) {
+            $file = CFLCatalog::model()->cache(1000)->findByPk($item_id);
+            /** @var CFLCatalog $file */
+            if ($file){
+                if ($file->sgroup == 1) {
+                    $letter = strtolower($file->dir[0]);
+                    if (($letter >= '0') && ($letter <= '9')) {
+                        $letter = '0';
+                        $file->dir = '0-999/' . $file->dir;
+                    } else
+                        $file->dir = $letter . '/' . $file->dir;
+                }
+                $files = array($file->dir.'/'.$file->name);
+                echo serialize($files);
+            }
+        }
+    }
+
+    public function actionFileGroupPath($item_id=0, $sg=2 ){
+        if ($item_id > 0) {
+            $files = CFLCatalog::model()->cache(1000)->findAllByAttributes(array('group'=>$item_id,'sgroup'=>$sg),array('order'=>'id'));
+            $res = array();
+            foreach($files as $file){
+                if ($file['sgroup'] == 1) {
+                    $letter = strtolower($file['dir'][0]);
+                    if (($letter >= '0') && ($letter <= '9')) {
+                        $letter = '0';
+                        $file['dir'] = '0-999/' . $file['dir'];
+                    } else
+                        $file['dir'] = $letter . '/' . $file['dir'];
+                }
+                $res[]= $file['dir'].'/'.$file['name'];
+            }
+            echo serialize($res);
+        }
+    }
+}
--- protected/controllers/CatalogController.php
+++ protected/controllers/CatalogController.php
@@ -259,57 +259,7 @@ class CatalogController extends Controller
          else $this->redirect('/users/login');
     }
 
-    public function actionGroupOfFile($id=0){
-        if ($id > 0) {
-            $file = CFLCatalog::model()->cache(100)->findByPk($id);
-            $files = CFLCatalog::model()->cache(10)->findAllByAttributes(array('group'=>$file->group,'sgroup'=>$file->sgroup));
-            /** @var CFLCatalog $file */
-            if ($file && !empty($files)){
-                $data=array('group'=>$file->group,'count'=>count($files));
-                foreach ($files as $f)
-                    $data['files'][]=$f['id'];
-                echo serialize($data);
-            }
-        }
-    }
 
-    public function actionFilePath($item_id=0){
-        if ($item_id > 0) {
-            $file = CFLCatalog::model()->cache(1000)->findByPk($item_id);
-            /** @var CFLCatalog $file */
-            if ($file){
-                if ($file->sgroup == 1) {
-                    $letter = strtolower($file->dir[0]);
-                    if (($letter >= '0') && ($letter <= '9')) {
-                        $letter = '0';
-                        $file->dir = '0-999/' . $file->dir;
-                    } else
-                        $file->dir = $letter . '/' . $file->dir;
-                }
-                $files = array($file->dir.'/'.$file->name);
-                echo serialize($files);
-            }
-        }
-    }
-
-    public function actionFileGroupPath($item_id=0, $sg=2 ){
-        if ($item_id > 0) {
-            $files = CFLCatalog::model()->cache(1000)->findAllByAttributes(array('group'=>$item_id,'sgroup'=>$sg),array('order'=>'id'));
-            $res = array();
-            foreach($files as $file){
-                if ($file['sgroup'] == 1) {
-                    $letter = strtolower($file['dir'][0]);
-                    if (($letter >= '0') && ($letter <= '9')) {
-                        $letter = '0';
-                        $file['dir'] = '0-999/' . $file['dir'];
-                    } else
-                        $file['dir'] = $letter . '/' . $file['dir'];
-                }
-                $res[]= $file['dir'].'/'.$file['name'];
-            }
-            echo serialize($res);
-        }
-    }
 
     public function actionDeleteFiles(){
         if (Yii::app()->user->id){
