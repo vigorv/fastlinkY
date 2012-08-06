1d5267825268e921a33e96fc80888a4defbaa6bd
snow <snow@diamondc.ru>
2012-08-06 12:40:46 +0700

    set cloud ready flag

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -84,4 +84,11 @@ class ApiController extends Controller
             echo serialize($id_list);
         }
     }
+
+    public function actionCloudReady($id=0,$sg=2){
+        // TO DO: hash key
+        $file = CFLCatalog::model()->findAllByAttributes(array('id'=>$id,'sgroup'=>$sg));
+        $file -> cloud_ready = 1;
+        $file->save();
+    }
 }
