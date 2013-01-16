640d2b0666aa0410dd4b6d063b733cbf8b792408
snow <snow@diamondc.ru>
2013-01-16 11:04:45 +0700

    create form

---
+++ protected/modules/admin/views/catalog/_form_create.php
@@ -0,0 +1,62 @@
+<div class="form">
+
+    <?php $form=$this->beginWidget('CActiveForm', array(
+    'id'=>'cflcatalog-form',
+    'enableAjaxValidation'=>false,
+)); ?>
+
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
+
+    <?php echo $form->errorSummary($model); ?>
+
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'title'); ?>
+        <?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>255)); ?>
+        <?php echo $form->error($model,'title'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'original_name'); ?>
+        <?php echo $form->textField($model,'original_name',array('size'=>60,'maxlength'=>255)); ?>
+        <?php echo $form->error($model,'original_name'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'name'); ?>
+        <?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255)); ?>
+        <?php echo $form->error($model,'name'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'comment'); ?>
+        <?php echo $form->textArea($model,'comment',array('rows'=>6, 'cols'=>50)); ?>
+        <?php echo $form->error($model,'comment'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'group'); ?>
+        <?php echo $form->textField($model,'group',array('size'=>10,'maxlength'=>10)); ?>
+        <?php echo $form->error($model,'group'); ?>
+    </div>
+
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'dir'); ?>
+        <?php echo $form->textField($model,'dir',array('size'=>60,'maxlength'=>255)); ?>
+        <?php echo $form->error($model,'dir'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'sgroup'); ?>
+        <?php echo $form->textField($model,'sgroup',array('size'=>10,'maxlength'=>10)); ?>
+        <?php echo $form->error($model,'sgroup'); ?>
+    </div>
+
+    <div class="row buttons">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+
+</div><!-- form -->
--- protected/modules/admin/views/catalog/create.php
+++ protected/modules/admin/views/catalog/create.php
@@ -12,4 +12,4 @@ $this->menu=array(
 
 <h1>Create CFLCatalog</h1>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php echo $this->renderPartial('_form_create', array('model'=>$model)); ?>
